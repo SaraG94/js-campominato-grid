49c7911b86c821fb995f7bf8f1c1bbc942289468
SaraG94 <sara.gherbassi.94@gmail.com>
2023-02-09 16:01:35 +0100

    START

---
+++ js/app.js
@@ -0,0 +1,9 @@
+console.log('ciaoo');
+
+//prendere il tasto play
+
+//al click su play si genera griglia
+//creare griglia
+//prendere celle
+//al click sulle celle
+  //stampiamo in console il numero della casella
