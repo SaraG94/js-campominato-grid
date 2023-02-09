9635b7901cd8f8ee2d3b5d095a0da289d442775f
SaraG94 <sara.gherbassi.94@gmail.com>
2023-02-09 17:54:47 +0100

    FEAT creat element

--- js/app.js
+++ js/app.js
@@ -9,11 +9,36 @@ console.log(buttonElement);
 buttonElement.addEventListener('click',function(){
     console.log('click');
 
+    //creare griglia
     const grigliaElement = document.getElementById('griglia');
     console.log(grigliaElement);
+    
+    //definire lato e celle
+    let latoGriglia = 10;
+    console.log(latoGriglia);
+
+    let numeroCelle = latoGriglia * latoGriglia;
+    console.log(numeroCelle);
+
+    for (let i = 0; i < numeroCelle; i++) {
+        let num = i + 1
+        console.log(num)
+
+        let divElement = document.createElement('div')
+        
+        //aggiungere classe
+        divElement.classList.add('celle');
+
+        //aggiungere style 
+        divElement.setAttribute("style", "width:calc(100% / 10)");
+        console.log(divElement);
+       
+        
+    }
+
 })
 
-//creare griglia
+
 //prendere celle
 //al click sulle celle
   //stampiamo in console il numero della casella
