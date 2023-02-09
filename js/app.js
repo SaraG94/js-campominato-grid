dd1a4ebabbc237a09bd323a2417f37a1fbe8e775
SaraG94 <sara.gherbassi.94@gmail.com>
2023-02-09 16:41:17 +0100

    FEAT button + Listener js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
 console.log('ciaoo');
 
-//prendere il tasto play
+//prendere il tasto Gioca
+const buttonElement = document.getElementById('pulsante');
+console.log(buttonElement);
 
 //al click su play si genera griglia
+
+buttonElement.addEventListener('click',function(){
+    console.log('click');
+})
+
 //creare griglia
 //prendere celle
 //al click sulle celle
